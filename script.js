f6ecef26cb5b385b7a746a51d5baaf7c5c0010b1
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-07 19:42:43 +0530

    added circle follow mouse pointer effect

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove", function(dets){
+        this.document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
+    })
+}
+circleMouseFollower()
