e636325f679cfaa04f062152c0cbe45c0d4513d5
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-08 12:29:50 +0530

    added animation to hero section headings

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function firstPageAnim(){
+    var tl = gsap.timeline();
+    tl.from("#nav",{
+        y: '-10',
+        opacity:0,
+        duration:1.5,         
+        ease: Expo.easeInOut
+    })
+    .to(".boundingelem",{
+        y:0,
+        ease:Expo.easeInOut,
+        duration:2,
+        stagger:0.2
+    })
+}
+
 function circleMouseFollower(){
     window.addEventListener("mousemove", function(dets){
         this.document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
     })
 }
 circleMouseFollower()
+firstPageAnim()
